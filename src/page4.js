683b8238a19748a65ffc0a2f1fc219704379d103
nathan <nathanlam650@gmail.com>
2022-07-21 17:09:13 +0800

    .

--- src/page4.js
+++ src/page4.js
@@ -2,28 +2,31 @@ function Page4() {
   return (
     <div className='smallerpage'>
     <div className="page4">
-    <Miniappcell appname={'jonh'}/>
 
-    <Miniappcell appname={'jonh'}/>
-    <Miniappcell appname={'jonh'}/>
-    <Miniappcell appname={'jonh'}/>
-    <Miniappcell appname={'jonh'}/>
-    <Miniappcell appname={'jonh'}/>
-    <Miniappcell appname={'jonh'}/>
-    <Miniappcell appname={'jonh'}/>    
-
+    <Miniappcell appname={'jonh'} appiconurl={'miniappicon2.png'}/>    
+    <Miniappcell appname={'jonh'} appiconurl={'miniappicon2.png'}/>    
+    <Miniappcell appname={'jonh'} appiconurl={'miniappicon2.png'}/>    
+    <Miniappcell appname={'jonh'} appiconurl={'miniappicon2.png'}/>    
+    <Miniappcell appname={'jonh'} appiconurl={'miniappicon2.png'}/>    
+    <Miniappcell appname={'jonh'} appiconurl={'miniappicon2.png'}/>    
+    <Miniappcell appname={'jonh'} appiconurl={'miniappicon2.png'}/>    
+    <Miniappcell appname={'jonh'} appiconurl={'miniappicon2.png'}/> 
     </div>
     </div>
   );
 }
 
 function Miniappcell(props){
+  const appiconurl22 = props.appiconurl;
+
   return(
   <div className="miniappcell">
   
   <div className="miniappicon">
-    <img src="miniappicon2.png"width={'80vh'} height={'80vh'} alt="defaulticon" />
+  <img src={appiconurl22} width={'80vh'} height={'80vh'} alt="defaulticon" />
   </div>
+
+
   appname:{props.appname}<br/>
 
 
